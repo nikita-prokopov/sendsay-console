11e2091a5d07915d43dfdc9a0929efd50f1355ac
nikita-prokopov <nikita-prokopov@bk.ru>
2020-07-02 15:21:03 +0300

    Updates

---
+++ precache-manifest.d9ec654ea246508897c0068227b33ac4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7d55b94a7583ea522afb71dd8585d9",
+    "url": "/console/index.html"
+  },
+  {
+    "revision": "840301212aba3e58835a",
+    "url": "/console/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8331034b32a6bf08da63",
+    "url": "/console/static/css/main.b374a04e.chunk.css"
+  },
+  {
+    "revision": "840301212aba3e58835a",
+    "url": "/console/static/js/2.03c4f5f1.chunk.js"
+  },
+  {
+    "revision": "296362f3435e3c4e1832bddadf6a9199",
+    "url": "/console/static/js/2.03c4f5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8331034b32a6bf08da63",
+    "url": "/console/static/js/main.91f8646a.chunk.js"
+  },
+  {
+    "revision": "771969bf365805030d10",
+    "url": "/console/static/js/runtime-main.ed7b0647.js"
+  },
+  {
+    "revision": "b60017629da250ca90d8b767e9247181",
+    "url": "/console/static/media/align-right.b6001762.svg"
+  },
+  {
+    "revision": "54d8dbc0fb2c398ade827b3be730629c",
+    "url": "/console/static/media/colon.54d8dbc0.svg"
+  },
+  {
+    "revision": "e0c567bba2ae106b95744b4ef8009ab7",
+    "url": "/console/static/media/log-out.e0c567bb.svg"
+  },
+  {
+    "revision": "aeb66b10d516e02c84324e3bfa1b8e4d",
+    "url": "/console/static/media/meh-face.aeb66b10.svg"
+  },
+  {
+    "revision": "b9d973b0e8bddd1a38bc4c8317bacedd",
+    "url": "/console/static/media/remove-fullscreen.b9d973b0.svg"
+  },
+  {
+    "revision": "11ebfce3a0b1c1c81fedbd31d75314d2",
+    "url": "/console/static/media/set-fullscreen.11ebfce3.svg"
+  }
+]);
