f719676d8ec59d97b929ef5249dd7941146be0de
nikita-prokopov <nikita-prokopov@bk.ru>
2020-07-04 11:55:21 +0300

    Updates

---
+++ precache-manifest.816078786ca658454914b5295cb3f8b3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167ea393ff403b25ec7f4fb6a99a4459",
+    "url": "/sendsay-console/index.html"
+  },
+  {
+    "revision": "6dc88069861b11ed3e9f",
+    "url": "/sendsay-console/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "170818cda44ae8a2b4d8",
+    "url": "/sendsay-console/static/css/main.15ccd233.chunk.css"
+  },
+  {
+    "revision": "6dc88069861b11ed3e9f",
+    "url": "/sendsay-console/static/js/2.907bad32.chunk.js"
+  },
+  {
+    "revision": "296362f3435e3c4e1832bddadf6a9199",
+    "url": "/sendsay-console/static/js/2.907bad32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170818cda44ae8a2b4d8",
+    "url": "/sendsay-console/static/js/main.2e498f33.chunk.js"
+  },
+  {
+    "revision": "1599f40784dc9c7bb987",
+    "url": "/sendsay-console/static/js/runtime-main.1be8e2dc.js"
+  },
+  {
+    "revision": "b60017629da250ca90d8b767e9247181",
+    "url": "/sendsay-console/static/media/align-right.b6001762.svg"
+  },
+  {
+    "revision": "54d8dbc0fb2c398ade827b3be730629c",
+    "url": "/sendsay-console/static/media/colon.54d8dbc0.svg"
+  },
+  {
+    "revision": "e0c567bba2ae106b95744b4ef8009ab7",
+    "url": "/sendsay-console/static/media/log-out.e0c567bb.svg"
+  },
+  {
+    "revision": "fa5f377b81df3b435037ed56d0df1667",
+    "url": "/sendsay-console/static/media/meh-face.fa5f377b.svg"
+  },
+  {
+    "revision": "b9d973b0e8bddd1a38bc4c8317bacedd",
+    "url": "/sendsay-console/static/media/remove-fullscreen.b9d973b0.svg"
+  },
+  {
+    "revision": "11ebfce3a0b1c1c81fedbd31d75314d2",
+    "url": "/sendsay-console/static/media/set-fullscreen.11ebfce3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/console/precache-manifest.6e27b97e1dc91c961f65d1ceeb26e04d.js"
+  "/sendsay-console/precache-manifest.816078786ca658454914b5295cb3f8b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/console/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sendsay-console/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
